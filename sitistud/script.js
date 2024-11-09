2de2f272ffe118469d129bc81161a19d0f7e81ef
palomba <palomba@istitutostradivari.it>
2024-11-09 13:13:35 +0000

    mobile menu

--- sitistud/script.js
+++ sitistud/script.js
@@ -1,6 +1,11 @@
 const sidebar = document.querySelector(".sidebar");
 const iframe = document.getElementById("mainIframe");
-const url = "https://script.google.com/macros/s/AKfycbzpUVk3t742shuNKbhd77oPHjR94gkwqycavm_8xqajJFC_VTnjEexzPImpH58O79Fj3w/exec?k=9XXSTi3hE3CGHBskc5bu";
+//const url = "https://script.google.com/macros/s/AKfycbzpUVk3t742shuNKbhd77oPHjR94gkwqycavm_8xqajJFC_VTnjEexzPImpH58O79Fj3w/exec?k=9XXSTi3hE3CGHBskc5bu";
+const url = "https://script.google.com/macros/s/AKfycbytqhT0vrxACZJdmdGRfd_YEa3sgZC4ctnWB8LKTDZbd9P_eoVjSfhJFkMmQFssu6Iy2w/exec?k=9XXSTi3hE3CGHBskc5bu";
+
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
 
 function initializeSidebar(sections) {
     sections.forEach((section, sectionIndex) => {
@@ -8,13 +13,14 @@ function initializeSidebar(sections) {
         titleElement.textContent = section.title;
         sidebar.appendChild(titleElement);
 
-        section.links.forEach((name, linkIndex) => {
+        section.links.forEach((lk, linkIndex) => {
             const linkElement = document.createElement("a");
-            linkElement.textContent = name.replace(/\b\w/g, (char) => char.toUpperCase());
+            linkElement.textContent = lk.base.replace(/\b\w/g, (char) => char.toUpperCase());
             linkElement.href = '#';
             linkElement.addEventListener("click", (event) => {
                 event.preventDefault();
-                iframe.src = `https://${name}.github.io`;
+                document.getElementById("sidebar").classList.remove("active");
+                iframe.src = `https://${lk.base}.github.io/${lk.page}`;
             });
             sidebar.appendChild(linkElement);
         });
